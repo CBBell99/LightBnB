c3df907de7702e9aadf397c98fc5c95e5a745056
Chris Bell <chrisbeckertonbell@gmail.com>
2022-02-10 15:03:32 -0800

    getAllReservations implemented

--- lightbnb_webapp/server/database.js
+++ lightbnb_webapp/server/database.js
@@ -89,8 +89,39 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+
+
+
+//const getAllReservations = function(guest_id, limit = 10) {
+
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return pool.query(
+//     `SELECT reservations.*, properties.title, properties.cost_per_night, properties.thumbnail_photo_url, properties.number_of_bedrooms, properties.number_of_bathrooms, properties.parking_spaces, avg(property_reviews.rating) as average_rating
+//   FROM reservations
+//   JOIN properties ON properties.id = reservations.property_id
+//   JOIN property_reviews ON properties.id = property_reviews.property_id
+//   WHERE reservations.guest_id = $1
+//   GROUP BY properties.id, reservations.id
+//   LIMIT $2;`, [guest_id, limit])
+//     .then(res => {
+//       return res.rows
+//     })
+//     .catch((err) => err.message);
+// }
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON properties.id = reservations.property_id
+    JOIN property_reviews ON reservations.id = property_reviews.reservation_id
+    WHERE reservations.guest_id = $1
+    AND reservations.start_date !=  now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2`
+  return pool.query(queryString, [guest_id, limit])
+    .then(res => {
+      return res.rows
+    })
 }
 exports.getAllReservations = getAllReservations;
 
