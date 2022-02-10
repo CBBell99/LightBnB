87e3f4b85d52817cd64fc685d78b82b44419039b
Chris Bell <chrisbell@Chriss-MacBook-Air.local>
2022-02-10 13:27:31 -0800

    add addUser query

--- lightbnb_webapp/server/database.js
+++ lightbnb_webapp/server/database.js
@@ -60,11 +60,26 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const name = user.name;
+  const email = user.email;
+  const password = user.password;
+  const queryString = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3) 
+    RETURNING * `
+  return pool.query(queryString, [name, email, password])
+    .then(res => {
+      return res.rows[0]
+    })
+    .catch(err => err.message)
 }
+
+// const addUser = function(user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
 exports.addUser = addUser;
 
 /// Reservations
